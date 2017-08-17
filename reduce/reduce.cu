886fcc53d36d6de36cb051b8c6cd468aa2039214
Wei Bai <baiwei0427@users.noreply.github.com>
2017-08-17 16:46:28 +0800

    Refactor
    1. Add two new kernel functions.
    2. Refactor reduce function to run many iterations

--- reduce/reduce.cu
+++ reduce/reduce.cu
@@ -3,6 +3,7 @@
 #include <math.h>
 #include <time.h>
 
+// Interleaved addressing with divergent branching
 __global__ void reduce_kernel0(int *d_out, int *d_in)
 {
     extern __shared__ int s_data[];
@@ -32,22 +33,84 @@ __global__ void reduce_kernel0(int *d_out, int *d_in)
     }
 }
 
+// Interleaved addressing with bank conflicts
+__global__ void reduce_kernel1(int *d_out, int *d_in)
+{
+    extern __shared__ int s_data[];
+
+    // thread ID inside the block
+    unsigned int tid = threadIdx.x;
+    // global ID across all blocks
+    unsigned int gid = blockIdx.x * blockDim.x + threadIdx.x;
+
+    // Copy elements from global memoery into per-block shared memory
+    s_data[tid] = d_in[gid];
+    // Ensure all elements have been copied into shared memory
+    __syncthreads();    
+
+    // s = 1, 2, 4, 8, ..... blockDim.x / 2
+    for (unsigned int s = 1; s < blockDim.x; s = (s << 1)) {
+        int index = tid * s * 2;
+
+        if (index + s < blockDim.x) {
+            s_data[index] += s_data[index + s];
+        }
+
+        // Ensure all threads in the block finish add in this round
+        __syncthreads();
+    }
+
+    if (tid == 0) {
+        d_out[blockIdx.x] = s_data[0];
+    }
+}
+
+// Sequential addressing
+__global__ void reduce_kernel2(int *d_out, int *d_in)
+{
+    extern __shared__ int s_data[];
+
+    // thread ID inside the block
+    unsigned int tid = threadIdx.x;
+    // global ID across all blocks
+    unsigned int gid = blockIdx.x * blockDim.x + threadIdx.x;
+
+    // Copy elements from global memoery into per-block shared memory
+    s_data[tid] = d_in[gid];
+    // Ensure all elements have been copied into shared memory
+    __syncthreads();    
+
+    // s = blockDim.x / 2, ....., 8, 4, 2, 1
+    for (unsigned int s = (blockDim.x >> 1); s >= 1; s = (s >> 1)) {
+        if (tid < s) {
+            s_data[tid] += s_data[tid + s];
+        }
+        // Ensure all threads in the block finish add in this round
+        __syncthreads();
+    }
+
+    if (tid == 0) {
+        d_out[blockIdx.x] = s_data[0];
+    }
+}
+
 inline bool is_power_of_2(int n)
 {
     return ((n & (n - 1)) == 0);
 }
 
 
-// input: array (in host memory) and array size 
-int reduce(int *h_in, int array_size)
+// input: array (in host memory), array size, iterations, expected result 
+void reduce(int *h_in, int array_size, int iters, int expected_result)
 {
-    int result = 0;
     // # of threads per block. It should be the power of two
     int threads = 1 << 10;
     // # of blocks in total. 
     int blocks = 1;
     // GPU memory pointers
     int *d_in, *d_intermediate, *d_out;
+    // final result in host memory
+    int h_out;
 
     if (!h_in || array_size <= 0 || !is_power_of_2(array_size))
         goto out;
@@ -62,27 +125,29 @@ int reduce(int *h_in, int array_size)
         goto out;
     
 
-    //printf("Shared memory per block in bytes: %d\n", threads * sizeof(int));
     // copy the input array from the host memory to the GPU memory
     cudaMemcpy(d_in, h_in, array_size * sizeof(int), cudaMemcpyHostToDevice);
-    // first stage reduce
-    reduce_kernel0<<<blocks, threads, threads * sizeof(int)>>>(d_intermediate, d_in);
-    
-    threads = blocks;
-    blocks = 1;
 
-    //printf("Shared memory per block in bytes: %d\n", threads * sizeof(int));
-    // second stage reduce    
-    reduce_kernel0<<<blocks, threads, threads * sizeof(int)>>>(d_out, d_intermediate);
+    // run many times
+    for (int i = 0; i < iters; i++) {
+        // first stage reduce
+        reduce_kernel2<<<blocks, threads, threads * sizeof(int)>>>(d_intermediate, d_in);
+        // second stage reduce    
+        reduce_kernel2<<<1, blocks, threads * sizeof(int)>>>(d_out, d_intermediate);
+    }
+
     // copy the result from the GPU memory to the host memory
-    cudaMemcpy(&result, d_out, sizeof(int), cudaMemcpyDeviceToHost);   
+    cudaMemcpy(&h_out, d_out, sizeof(int), cudaMemcpyDeviceToHost);
+
+    if (h_out != expected_result) {
+        printf("Wrong result: %d (expected) %d (actual)\n", expected_result, h_out);
+    }
 
 out:
     // free GPU memory
     cudaFree(d_in);
     cudaFree(d_intermediate);
     cudaFree(d_out);
-    return result;
 }
 
 // generate a random integer in [min, max]
@@ -110,19 +175,13 @@ int main()
         sum += h_in[i];
     }
 
-    const int iters = 50;
     cudaEvent_t start, stop;
     cudaEventCreate(&start);
     cudaEventCreate(&stop);
     
     cudaEventRecord(start, 0);
-    for (int i = 0; i < iters; i++) {
-        // wrong result
-        int result = reduce(h_in, ARRAY_SIZE);
-        if (result != sum) {
-            printf("Wrong result: %d and %d\n", sum, result);
-        }
-    }
+    const int iters = 1000;
+    reduce(h_in, ARRAY_SIZE, iters, sum);
     cudaEventRecord(stop, 0);
     cudaEventSynchronize(stop);
 
