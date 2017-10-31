2b84b8083465ae74b0a0860f959658c40b66f5e5
Wei Bai <baiwei0427@users.noreply.github.com>
2017-10-31 14:00:50 +0800

    Profile pinned memcpy overhead

--- memcpy-measure/main.cu
+++ memcpy-measure/main.cu
@@ -10,12 +10,63 @@ void usage(char *program)
         fprintf(stderr, "    -a     : set CPU affinity\n");
 }
 
+// copy size worth of bytes from h_in to d, then from d back to h_out for iters rounds
+void profile_memcpy(char *h_in, char *h_out, char *d, int size, int iters)
+{
+        cudaEvent_t start, stop;
+        float time, h2d_total_time, d2h_total_time, throughput, avg_time;
+        int i;
+
+        cudaEventCreate(&start);
+        cudaEventCreate(&stop);
+        h2d_total_time = d2h_total_time = 0;
+
+        for (i = 0; i < iters; i++) {
+                // memory transfer from host to device
+                cudaEventRecord(start, 0);
+                if (cudaMemcpy(d, h_in, size, cudaMemcpyHostToDevice) != cudaSuccess) {
+                        fprintf(stderr, "Error: memcpy from host to device\n");
+                }
+                cudaEventRecord(stop, 0);
+                cudaEventSynchronize(stop);
+                cudaEventElapsedTime(&time, start, stop);
+                h2d_total_time += time;    
+
+                // memory transfer from device to host
+                cudaEventRecord(start, 0);
+                if (cudaMemcpy(h_out, d, size, cudaMemcpyDeviceToHost) != cudaSuccess) {
+                        fprintf(stderr, "Error: memcpy from device to host\n");
+                }
+                cudaEventRecord(stop, 0);
+                cudaEventSynchronize(stop);
+                cudaEventElapsedTime(&time, start, stop);
+                d2h_total_time += time;     
+        }
+
+        // calculate host to device information
+        avg_time = h2d_total_time / iters / 1000;  // time in second
+        throughput = (float)size / avg_time / 1000000000; // throughput in GB/s
+
+        printf("  Host to Device Time: %.6f s\n", avg_time);
+        printf("  Host to Device Throughput: %.6f GB/s\n", throughput);
+
+        // calculate device to host information
+        avg_time = d2h_total_time / iters / 1000;  // time in second
+        throughput = (float)size / avg_time / 1000000000; // throughput in GB/s
+
+        printf("  Device to Host Time: %.6f s\n", avg_time);
+        printf("  Device to Host Throughput: %.6f GB/s\n", throughput);
+
+        cudaEventDestroy(start);
+        cudaEventDestroy(stop);
+}
+
 int main(int argc, char **argv)
 {
-        int size, iters, i;
-        char *h_a, *h_b, *d;    // host and device memory
-        cudaEvent_t start, stop;
-        float time, cumulative_h2d_time = 0.f, cumulative_d2h_time = 0.f, throughput, avg_time;
+        int size, iters;
+        char *h_in_pageable, *h_out_pageable;   // host pageable memory 
+        char *h_in_pinned, *h_out_pinned;       // host pinned memory 
+        char *d;        // device memory
         bool set_affinity = false;     
         cpu_set_t  mask;
 
@@ -46,67 +97,54 @@ int main(int argc, char **argv)
                 }
         }
 
-        // allocate host memory
-        h_a = (char*)malloc(size);
-        h_b = (char*)malloc(size);
-        if (!h_a || !h_b) {
-                fprintf(stderr, "Error: allocate host memory\n");
-                free(h_a);
-                free(h_b);
+        // allocate host pageable memory
+        h_in_pageable = (char*)malloc(size);
+        h_out_pageable = (char*)malloc(size);
+        if (!h_in_pageable || !h_out_pageable) {
+                fprintf(stderr, "Error: allocate host pageable memory\n");
+                free(h_in_pageable);
+                free(h_out_pageable);
                 return EXIT_FAILURE;
         }
+
+        // allocate host pinned memory
+        if (cudaMallocHost((void**)&h_in_pinned, size) != cudaSuccess ||
+            cudaMallocHost((void**)&h_out_pinned, size) != cudaSuccess) {
+                fprintf(stderr, "Error: allocate host pinned memory\n");
+                free(h_in_pageable);
+                free(h_out_pageable);
+                cudaFreeHost(h_in_pinned);
+                cudaFreeHost(h_out_pinned);
+                return EXIT_FAILURE;                                    
+        } 
 
         // allocate device memory
         if (cudaMalloc(&d, size) != cudaSuccess) {
                 fprintf(stderr, "Error: allocate device memory\n"); 
-                free(h_a);
-                free(h_b);        
+                free(h_in_pageable);
+                free(h_out_pageable);
+                cudaFreeHost(h_in_pinned);
+                cudaFreeHost(h_out_pinned);   
                 return EXIT_FAILURE;
         }
 
+        // warm up
         cudaFree(0);
-        cudaEventCreate(&start);
-        cudaEventCreate(&stop);
 
-        for (i = 0; i < iters; i++) {
-                // memory transfer from host to device
-                cudaEventRecord(start, 0);
-                if (cudaMemcpy(d, h_a, size, cudaMemcpyHostToDevice) != cudaSuccess) {
-                        fprintf(stderr, "Error: memcpy from host to device\n");
-                }
-                cudaEventRecord(stop, 0);
-                cudaEventSynchronize(stop);
-                cudaEventElapsedTime(&time, start, stop);
-                cumulative_h2d_time += time;    
+        // Profile memory copy
+        printf("Transfer size (MB): %f\n\n", (float)size / (1024 * 1024));
+        printf("Pageable transfers\n");
+        profile_memcpy(h_in_pageable, h_out_pageable, d, size, iters);
+        printf("\n");
+        printf("Pinned transfers\n");
+        profile_memcpy(h_in_pinned, h_out_pinned, d, size, iters);
 
-                // memory transfer from device to host
-                cudaEventRecord(start, 0);
-                if (cudaMemcpy(h_b, d, size, cudaMemcpyDeviceToHost) != cudaSuccess) {
-                        fprintf(stderr, "Error: memcpy from device to host\n");
-                }
-                cudaEventRecord(stop, 0);
-                cudaEventSynchronize(stop);
-                cudaEventElapsedTime(&time, start, stop);
-                cumulative_d2h_time += time;                    
-        }
-
-        free(h_a);
-        free(h_b);
+        // free memory
+        free(h_in_pageable);
+        free(h_out_pageable);
+        cudaFreeHost(h_in_pinned);
+        cudaFreeHost(h_out_pinned);   
         cudaFree(d);
-
-        // calculate host to device information
-        avg_time = cumulative_h2d_time / iters / 1000;  // time in second
-        throughput = (float)size / avg_time / 1000000000; // throughput in GB/s
-
-        printf("Host to Device Time: %.5f ms\n", avg_time);
-        printf("Host to Device Throughput: %.5f GB/s\n", throughput);
-
-        // calculate device to host information
-        avg_time = cumulative_d2h_time / iters / 1000;  // time in second
-        throughput = (float)size / avg_time / 1000000000; // throughput in GB/s
-
-        printf("Device to Host Time: %.5f ms\n", avg_time);
-        printf("Device to Host Throughput: %.5f GB/s\n", throughput);
 
         return EXIT_SUCCESS;
 }
